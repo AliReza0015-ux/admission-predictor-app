527d53108b6485828012102ff52cd86e408292a4
AliReza0015-ux <mohs0015@algonquinlive.com>
2025-08-03 03:40:53 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 import joblib
 
-def load_model(path="model/admission_model.pkl"):
-    return joblib.load(path)
+def load_model():
+    return joblib.load("admission_model.pkl")
 
 def predict_admission(model, input_df):
+    # Ensure the input columns match the training data
+    expected_cols = ['GRE Score', 'TOEFL Score', 'University Rating', 'SOP', 'LOR', 'CGPA', 'Research']
+    input_df = input_df[expected_cols]
     return model.predict(input_df)[0]
