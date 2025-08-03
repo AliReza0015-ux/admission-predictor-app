0375e7c9ca99de9960f7d8f4111eaccf4bab4b69
AliReza0015-ux <mohs0015@algonquinlive.com>
2025-08-03 03:27:07 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-def preprocess_input(gre, toefl, university_rating, sop, lor, cgpa, research):
-    data = {
-        "GRE Score": [gre],
-        "TOEFL Score": [toefl],
-        "University Rating": [university_rating],
-        "SOP": [sop],
-        "LOR": [lor],
-        "CGPA": [cgpa],
-        "Research": [research]
-    }
-    return pd.DataFrame(data)
+def preprocess_input(gre, toefl, rating, sop, lor, cgpa, research):
+    return pd.DataFrame([{
+        "GRE Score": gre,
+        "TOEFL Score": toefl,
+        "University Rating": rating,
+        "SOP": sop,
+        "LOR": lor,
+        "CGPA": cgpa,
+        "Research": research
+    }])
